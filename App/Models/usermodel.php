14f52cb2f74776e449c8d2673a343a22bd9cf52b
faza1906 <234260021.mhs@stmikjabar.ac.id>
2025-05-22 14:43:39 +0700

    Add files via upload

--- App/Models/usermodel.php
+++ App/Models/usermodel.php
@@ -1,1 +1,17 @@
+<?php
 
+// app/Models/UserModel.php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model {
+    // Nama tabel di database yang digunakan oleh model ini
+    protected $table = 'users';
+
+    // Kolom-kolom yang diizinkan untuk diisi secara massal (misal pakai ->save() atau ->insert())
+    protected $allowedFields = ['username', 'password'];
+
+    // Tidak menggunakan kolom timestamps (created_at & updated_at)
+    protected $useTimestamps = false;
+}
